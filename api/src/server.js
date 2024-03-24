05aa422d5177e7deed4d063c1ba19389d91cbf1a
sirguy12 <ryankairo09@yahoo.com.br>
2024-03-24 16:18:49 -0300

    [build] added views pattern, api and frontend are merged

--- api/src/server.js
+++
@@ -1,8 +0,0 @@
-const server = require('./app')
-require('dotenv').config()
-const serverport = process.env.PORT
-
-server.listen(serverport, () =>{
-    console.log(`Servidor está rodando na porta ${serverport}`)
-})
-
