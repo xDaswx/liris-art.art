88eb1d2ded436062083f579d027df134fcc3eff6
dasw <dasw@localhost.localdomain>
2024-10-01 17:50:59 -0300

    [feat] draw controller

--- src/controllers/drawsController.js
+++ src/controllers/drawsController.js
@@ -1,21 +1,20 @@
-const express = require('express')
-const drawingsModel = require('../models/drawingsModel')
+const express = require('express');
+const drawingsModel = require('../models/drawingsModel');
 
-const getAllDrawings = async (req,res) => {
-    const drawings = await drawingsModel.getAllDrawings()
-    if (drawings.error) {
-        return res.status(500).send(drawings)
+const getAllDrawings = async (req, res) => {
+    try {
+        const drawings = await drawingsModel.find();
+        return res.status(200).send(drawings);
+    } catch (err) {
+        return res.status(500).send({ message: 'Internal Error', error: err.message });
     }
-    return res.status(200).send(drawings[0])
-}
+};
 
-const ping = async (req,res) => {
-    
-    return res.status(200).send({message:'pong'})
-}
-
+const ping = async (req, res) => {
+    return res.status(200).send({ message: 'pong' });
+};
 
 module.exports = {
     getAllDrawings,
-    ping
-}
+    ping,
+};
