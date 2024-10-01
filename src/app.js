efbfe5af7d1658637a12aa53110cbd8cfe6ba0a1
dasw <dasw@localhost.localdomain>
2024-10-01 17:49:37 -0300

    [feat] mongoose connection

--- src/app.js
+++ src/app.js
@@ -2,13 +2,17 @@ const express = require('express')
 const cors = require('cors')
 const routers = require('./router')
 const path = require('path')
+const connectDB = require('./models/dbconnection'); 
 const app = express()
+
 
 app.use(cors({
     origin: '*'
 }))
 
 app.use(express.static(path.join(__dirname, 'views')))
+
+connectDB();
 
 app.use(express.json())
 app.use(routers)
